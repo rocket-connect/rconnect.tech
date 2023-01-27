9ec2b19a506bc8fb16f4218f6755f461c70e1b57
daniel starns <danielstarns@hotmail.com>
2023-01-27 14:57:35 +0000

    style: mobile intro

--- frontend/src/components/views/Header.tsx
+++ frontend/src/components/views/Header.tsx
@@ -3,7 +3,7 @@ import { Logo } from "../utils/Logo";
 
 export function Header() {
   return (
-    <div className="bg-white w-full py-3 fixed z-10 border-b-rocket-connect-darkblue border-4">
+    <div className="bg-white w-full py-3 z-10 border-b-rocket-connect-darkblue border-4">
       <Container>
         <div className="flex justify-between content-center">
           <div className="w-10">
--- frontend/src/components/views/Intro.tsx
+++ frontend/src/components/views/Intro.tsx
@@ -4,19 +4,19 @@ import { Link } from "../utils/Link";
 
 export function Intro() {
   return (
-    <div className="bg-rocket-connect-darkblue w-full relative intro-stars overflow-hidden">
+    <div className="bg-rocket-connect-darkblue overflow-hidden">
       <div className="star star0"></div>
       <Container>
-        <div className="py-60">
-          <div className="w-3/5">
-            <h1 className="font-bold text-rocket-connect-lightgrey text-8xl text-white">
+        <div className="">
+          <div className="py-24 lg:py-32 w-100 lg:w-3/5">
+            <h1 className="text-center lg:text-left font-bold text-rocket-connect-lightgrey text-white text-3xl sm:text-5xl lg:text-6xl">
               The home of software solutions
             </h1>
-            <p className="w-3/5 text-rocket-connect-lightgrey mt-5 text-white text-2xl italic">
+            <p className="mt-5 mx-auto text-center lg:mx-0 lg:text-left w-3/5 text-rocket-connect-lightgrey text-white italic">
               At Rocket Connect our goal is to create software and tools for the
               benefit of society.
             </p>
-            <p className="mt-5 text-2xl">
+            <p className="mx-auto text-center lg:text-left mt-5">
               <Link
                 color="white"
                 content="Contact"
