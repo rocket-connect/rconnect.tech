a2b73f9ddb5fe1c0dc23aa6319e347da50d62b30
daniel starns <danielstarns@hotmail.com>
2023-01-27 13:23:07 +0000

    feat: add border to header

--- frontend/src/components/views/Header.tsx
+++ frontend/src/components/views/Header.tsx
@@ -4,7 +4,7 @@ import { Nav } from "../utils/Nav";
 
 export function Header() {
   return (
-    <div className="bg-white w-full py-3 fixed z-10">
+    <div className="bg-white w-full py-3 fixed z-10 border-b-rocket-connect-darkblue border-4">
       <Container>
         <div className="flex justify-between content-center">
           <div className="w-8">
