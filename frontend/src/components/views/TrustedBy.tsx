f094253eb643157488bf9b89683c3a7b13260849
daniel starns <danielstarns@hotmail.com>
2023-01-27 16:03:54 +0000

    style: mobile projects and trusted

--- frontend/src/components/views/TrustedBy.tsx
+++ frontend/src/components/views/TrustedBy.tsx
@@ -13,13 +13,13 @@ export function TrustedBy() {
   return (
     <Divider>
       <div className="flex align-center justify-center">
-        <p className="italic text-2xl text-rocket-connect-darkblue">
+        <p className="text-center italic text:sm xl:text-2xl text-rocket-connect-darkblue">
           Our team have been trusted by leading organizations
         </p>
       </div>
       <div className="flex align-center justify-between py-5">
         {trustedBy.map((item) => (
-          <img className="h-12 my-5" src={item.logo} />
+          <img className="h-4 md:h-8 xl:h-12 my-5" src={item.logo} />
         ))}
       </div>
     </Divider>
