968538c40bad18741259ea4cd46a5c4e751fae1d
danstarns <danielstarns@hotmail.com>
2023-07-18 21:00:51 +0700

    fix: intro text

--- frontend/src/components/views/Intro.tsx
+++ frontend/src/components/views/Intro.tsx
@@ -7,24 +7,22 @@ export function Intro() {
     <div className="pt-12 gradient-background bg-rocket-connect-darkblue overflow-hidden">
       <div className="star star0"></div>
       <Container>
-        <div className="">
-          <div className="py-24 lg:py-32 w-100 lg:w-3/5">
-            <h1 className="text-left font-bold text-rocket-connect-lightgrey text-white text-4xl xl:text-6xl">
-              The home of software solutions
-            </h1>
-            <p className="mt-5 text-rocket-connect-lightgrey text-white italic">
-              Our goal is to create software and tools for the benefit of
-              society.
-            </p>
-            <p className="mt-5">
-              <Link
-                color="white"
-                content="Contact"
-                href="email"
-                lightGrey={true}
-              ></Link>
-            </p>
-          </div>
+        <div className="py-24 lg:py-32 w-100 lg:w-3/5">
+          <h1 className="text-left font-bold text-rocket-connect-lightgrey text-white text-3xl xl:text-6xl">
+            We build APIs and open-source libraries.
+          </h1>
+          <p className="mt-5 text-rocket-connect-lightgrey text-white italic">
+            Rocket Connect's goal is to create software and tools for the
+            benefit of society.
+          </p>
+          <p className="mt-5">
+            <Link
+              color="white"
+              content="Contact"
+              href="email"
+              lightGrey={true}
+            ></Link>
+          </p>
         </div>
       </Container>
     </div>
