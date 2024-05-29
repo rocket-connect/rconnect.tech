876f5102fab78776ddfb97a4890a7587bcd39a29
danstarns <danielstarns@hotmail.com>
2024-05-29 23:58:25 +0100

    content: add youtube videos

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {images: {
+const nextConfig = {
+  images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'i.ytimg.com',
+        protocol: "https",
+        hostname: "i.ytimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img.youtube.com",
       },
     ],
-  },};
+  },
+};
 
 export default nextConfig;
