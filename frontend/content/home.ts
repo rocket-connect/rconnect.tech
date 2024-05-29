31ffa8f779887da844c7d07e9440c6b9c5aacd0f
danstarns <danielstarns@hotmail.com>
2024-05-29 22:57:04 +0100

    images: add raw images to use for services

--- frontend/content/home.ts
+++ frontend/content/home.ts
@@ -16,28 +16,28 @@ export const home = {
         name: "Open Source Development",
         content:
           "We are dedicated to advancing the open-source ecosystem by creating and maintaining high-quality projects. Our team actively contributes to open-source communities and ensures our projects are accessible on platforms like GitHub. You can hire our team to join your project and deliver high-quality solutions.",
-        image: "/images/library-development.jpg",
+        image: "/images/oss-dev.png",
         href: "/services",
       },
       {
         name: "Community Management",
         content:
           "Our expertise in community management helps foster a vibrant and engaged developer community. We organize and facilitate discussions, support forums, and events to promote knowledge sharing and collaboration. With our vast international network, we have solutions to meet all your organization's community needs.",
-        image: "/images/developer-relations.jpg",
+        image: "/images/community.jpg",
         href: "/services",
       },
       {
         name: "Developer Relations",
         content:
           "Our Developer Relations service helps your organization build and nurture relationships through strategic participation in conferences, workshops, and industry events. By fostering knowledge exchange and learning from peers, we ensure your organization remains at the forefront of industry trends and innovations.",
-        image: "/images/business-connections.jpg",
+        image: "/images/dev-rel.jpg",
         href: "/services",
       },
       {
         name: "Software Training",
         content:
           "We collaborate with your organization to build and deliver tailored training programs that meet your developer's needs. Utilizing the latest technologies, our training sessions ensure your developers stay ahead of industry trends and are equipped with the knowledge to excel.",
-        image: "/images/business-connections.jpg",
+        image: "/images/training.jpg",
         href: "/services",
       },
     ],
@@ -55,25 +55,25 @@ export const home = {
       {
         title: "Dan Starns: Working with Graphs and GraphQL",
         href: "https://youtu.be/aXq9OdW5kh0",
-        image: "/images/developer-relations.jpg",
+        image: "/images/training.jpg",
         desc: "Uncover the transformative potential of graphs in the realm of query languages. From optimizing data retrieval to enhancing analytical capabilities, this talk explores the synergy between graphs and query languages, shedding light on innovative approaches to data management.",
       },
       {
         title: "Dan Starns: Working with Graphs and GraphQL",
         href: "https://youtu.be/aXq9OdW5kh0",
-        image: "/images/developer-relations.jpg",
+        image: "/images/training.jpg",
         desc: "Uncover the transformative potential of graphs in the realm of query languages. From optimizing data retrieval to enhancing analytical capabilities, this talk explores the synergy between graphs and query languages, shedding light on innovative approaches to data management.",
       },
       {
         title: "Dan Starns: Working with Graphs and GraphQL",
         href: "https://youtu.be/aXq9OdW5kh0",
-        image: "/images/developer-relations.jpg",
+        image: "/images/training.jpg",
         desc: "Uncover the transformative potential of graphs in the realm of query languages. From optimizing data retrieval to enhancing analytical capabilities, this talk explores the synergy between graphs and query languages, shedding light on innovative approaches to data management.",
       },
       {
         title: "Dan Starns: Working with Graphs and GraphQL",
         href: "https://youtu.be/aXq9OdW5kh0",
-        image: "/images/developer-relations.jpg",
+        image: "/images/training.jpg",
         desc: "Uncover the transformative potential of graphs in the realm of query languages. From optimizing data retrieval to enhancing analytical capabilities, this talk explores the synergy between graphs and query languages, shedding light on innovative approaches to data management.",
       },
     ],
--- frontend/content/services.ts
+++ frontend/content/services.ts
@@ -12,28 +12,28 @@ export const services = {
         name: "Open Source Development",
         content:
           "We are dedicated to advancing the open-source ecosystem by creating and maintaining high-quality projects. Our team actively contributes to open-source communities and ensures our projects are accessible on platforms like GitHub. You can hire our team to join your project and deliver high-quality solutions.",
-        image: "/images/library-development.jpg",
+        image: "/images/oss-dev.png",
         href: "",
       },
       {
         name: "Community Management",
         content:
           "Our expertise in community management helps foster a vibrant and engaged developer community. We organize and facilitate discussions, support forums, and events to promote knowledge sharing and collaboration. With our vast international network, we have solutions to meet all your organization's community needs.",
-        image: "/images/developer-relations.jpg",
+        image: "/images/community.jpg",
         href: "",
       },
       {
         name: "Developer Relations",
         content:
           "Our Developer Relations service helps your organization build and nurture relationships through strategic participation in conferences, workshops, and industry events. By fostering knowledge exchange and learning from peers, we ensure your organization remains at the forefront of industry trends and innovations.",
-        image: "/images/business-connections.jpg",
+        image: "/images/dev-rel.jpg",
         href: "",
       },
       {
         name: "Software Training",
         content:
           "We collaborate with your organization to build and deliver tailored training programs that meet your developer's needs. Utilizing the latest technologies, our training sessions ensure your developers stay ahead of industry trends and are equipped with the knowledge to excel.",
-        image: "/images/business-connections.jpg",
+        image: "/images/training.jpg",
         href: "",
       },
     ],
