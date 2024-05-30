0b77570b423a1d08161b478e4ab870cbf656674d
danstarns <danielstarns@hotmail.com>
2024-05-30 22:55:49 +0100

    content: about wording

--- frontend/content/about.ts
+++ frontend/content/about.ts
@@ -10,12 +10,12 @@ export const about = {
     cta: { label: "Contact us", href: "/contact" },
   },
   intro:
-    "Rocket Connect puts people and connections at the forefront of its operations. Our goal is to connect people through our deep understanding of the world and open-source software. You may find us collaborating on popular GitHub repositories, speaking at your local tech conference, or connecting with somebody you know.",
+    "Rocket Connect puts people and connections at the forefront of its operations. Our goal is to connect people through our deep understanding of the world through open-source software. You may find us collaborating on popular GitHub repositories, speaking at your local tech conference, or connecting with somebody you know.",
   sections: [
     {
       title: "Where we started",
       content:
-        "Started by our founder in London, our team is now established all over the globe, embracing diverse cultures and backgrounds. This global presence allows us to leverage a wide range of perspectives and expertise, enriching our contributions to the open-source community.",
+        "Started by our founder in London, our journey began as a development agency. Over the years, we have scaled into providing comprehensive enterprise solutions and fostering community building. Our team is now established all over the globe, embracing diverse cultures and backgrounds. This global presence allows us to leverage a wide range of perspectives and expertise, enriching our contributions to the open-source community.",
       image: "/images/started.jpeg",
       imageDescription:
         "Rocket Connect founded in London 2019 by our founder Dan.",
@@ -23,23 +23,24 @@ export const about = {
     {
       title: "What we believe in",
       content:
-        "We believe in delivering great software and sharing it with the world, grounded in principles of philanthropy, fairness, and collaboration. We are committed to fostering many vibrant communities, building strong connections, and establishing meaningful partnerships.",
+        "We believe in delivering great software and sharing it with the world, grounded in principles of philanthropy, fairness, quality, transparency, and collaboration. We are committed to fostering many vibrant communities, building strong connections, and establishing meaningful partnerships. Most importantly, we believe in building solutions that customers love.",
       image: "/images/believe.jpg",
-      imageDescription: "Attendees of DevTools Bangkok working with HubQL.",
+      imageDescription: "Attendees learning at GraphQL Singapore.",
     },
     {
       title: "Our mission",
       content:
         "Our mission is to connect people through open source. We strive to build solutions that help the greater community and share our knowledge through our extensive networks. By leveraging our expertise and connections, we aim to make a positive impact on the open-source community and beyond.",
       image: "/images/mission.jpg",
-      imageDescription: "Attendees of DevTools Bangkok working with HubQL.",
+      imageDescription: "Our team teaching at Graph Databases Bangalore.",
     },
     {
       title: "Join us",
       content:
         "We are always looking for passionate individuals to join our mission. Whether you're a developer, community manager, or just someone who shares our values, we welcome you to be part of Rocket Connect. Together, we can continue to build and share open-source solutions that make a difference. Contact us today",
       image: "/images/join.jpg",
-      imageDescription: "Attendees of DevTools Bangkok working with HubQL.",
+      imageDescription:
+        "Attendees of DevTools Bangkok learning about API gateways.",
     },
   ],
 };
