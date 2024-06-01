1b40a074fee9b9dc9be7682a441411d810f965db
danstarns <danielstarns@hotmail.com>
2024-06-01 22:31:50 +0100

    style: opacity images and add border

--- frontend/components/templates/DirectorySection.tsx
+++ frontend/components/templates/DirectorySection.tsx
@@ -35,7 +35,7 @@ export const DirectorySection = ({ content }: { content: any }) => {
                 index: number
               ) => (
                 <CarouselItem key={"direvent" + index} className="lg:basis-1/4">
-                  <div className="relative w-[300px] h-[450px] bg-zinc-400 p-8 rounded-xl flex flex-col justify-between">
+                  <div className="relative w-[300px] h-[450px] bg-zinc-400 p-8 rounded-xl flex flex-col justify-between overflow-hidden border border-[#E6E6FF] dark:border-[#546C87]">
                     <div className="ml-auto relative z-20 flex flex-col gap-2 items-end">
                       {event.youtube !== "" && (
                         <a
@@ -66,13 +66,13 @@ export const DirectorySection = ({ content }: { content: any }) => {
                       height={80}
                     />
 
-                    <div className="absolute z-10 w-full h-full top-0 left-0">
+                    <div className="absolute z-10 w-full h-full top-0 left-0 ">
                       <Image
                         src={event.image}
                         alt=""
                         width={300}
                         height={450}
-                        className="w-full h-full"
+                        className="w-full h-full opacity-60 object-cover"
                       />
                     </div>
                   </div>
