e952d80e0328444d750acbc9be808681b2b41966
danstarns <danielstarns@hotmail.com>
2024-06-06 19:58:18 +0100

    feat: add link for home services

--- frontend/components/templates/ServicesCard.tsx
+++ frontend/components/templates/ServicesCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 interface CardProps {
   name: string;
@@ -23,26 +24,28 @@ export const ServicesCard = ({ cards }: CardsProps) => {
 
 const Card = ({ card }: any) => {
   return (
-    <div className="border border-[#EFEFEF] dark:border-[#1F344A] shadow-sm rounded-xl overflow-hidden  bg-background-main dark:bg-[#284360]">
-      <div className="relative aspec-video w-full">
-        <div className="relative z-20 mix-blend-multiply bg-[#D1DBFF] dark:bg-[#284360] h-full" />
-        <Image
-          src={card.image}
-          alt=""
-          width={432}
-          height={246}
-          className="relative z-10 w-full object-cover aspect-video"
-          priority
-        />
+    <Link href={card.href}>
+      <div className="border border-[#EFEFEF] dark:border-[#1F344A] shadow-sm rounded-xl overflow-hidden  bg-background-main dark:bg-[#284360] hover:text-[#EEEEFB] hover:dark:text-[#1A2735] hover:scale-[1.01] transition-all ease-in hover:shadow-lg hover:cursor-pointer">
+        <div className="relative aspec-video w-full">
+          <div className="relative z-20 mix-blend-multiply bg-[#D1DBFF] dark:bg-[#284360] h-full" />
+          <Image
+            src={card.image}
+            alt=""
+            width={432}
+            height={246}
+            className="relative z-10 w-full object-cover aspect-video"
+            priority
+          />
+        </div>
+        <div className="flex flex-col gap-2 items-start justify-center text-left text-foreground-main p-6">
+          <h3 className="text-foreground-main dark:text-foreground-invert text-lg font-medium truncate w-full">
+            {card.name}
+          </h3>
+          <p className=" text-foreground-main dark:text-foreground-invert line-clamp-3">
+            {card.content}
+          </p>
+        </div>
       </div>
-      <div className="flex flex-col gap-2 items-start justify-center text-left text-foreground-main p-6">
-        <h3 className="text-foreground-main dark:text-foreground-invert text-lg font-medium truncate w-full">
-          {card.name}
-        </h3>
-        <p className=" text-foreground-main dark:text-foreground-invert line-clamp-3">
-          {card.content}
-        </p>
-      </div>
-    </div>
+    </Link>
   );
 };
--- frontend/content/home.ts
+++ frontend/content/home.ts
@@ -10,28 +10,28 @@ export const home = {
         content:
           "We are dedicated to advancing the open-source ecosystem by creating and maintaining high-quality projects. Our team actively contributes to open-source communities and ensures our projects are accessible on platforms like GitHub. You can hire our team to join your project and deliver high-quality solutions.",
         image: "/images/oss-dev.png",
-        href: "/services",
+        href: "/services/#oss-dev",
       },
       {
         name: "Community Management",
         content:
           "Our expertise in community management helps foster a vibrant and engaged developer community. We organize and facilitate discussions, support forums, and events to promote knowledge sharing and collaboration. With our vast international network, we have solutions to meet all your organization's community needs.",
         image: "/images/community.jpg",
-        href: "/services",
+        href: "/services/#community-mgmt",
       },
       {
         name: "Developer Relations",
         content:
           "Our Developer Relations service helps your organization build and nurture relationships through strategic participation in conferences, workshops, and industry events. By fostering knowledge exchange and learning from peers, we ensure your organization remains at the forefront of industry trends and innovations.",
         image: "/images/dev-rel.jpg",
-        href: "/services",
+        href: "/services/#dev-rel",
       },
       {
         name: "Software Training",
         content:
           "We collaborate with your organization to build and deliver tailored training programs that meet your developer's needs. Utilizing the latest technologies, our training sessions ensure your developers stay ahead of industry trends and are equipped with the knowledge to excel.",
         image: "/images/training.jpg",
-        href: "/services",
+        href: "/services/#software-training",
       },
     ],
     cta: { label: "Get to know more", href: "/services" },
